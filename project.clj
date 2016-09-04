fa4f80fe4a2b363b50bdb790823e141a9272eb92
Joe Littlejohn <joelittlejohn@gmail.com>
2016-09-04 21:29:07 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-h2 "0.1.0"
+(defproject lein-h2 "0.1.1-SNAPSHOT"
   :description "A lein plugin that runs an h2 instance in server mode"
   :url "http://github.com/joelittlejohn/lein-h2"
   :license {:name "Eclipse Public License"
