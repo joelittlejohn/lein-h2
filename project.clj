fb0621cffd1a5e4d0b289ec615f6530f02b27ea8
Joe Littlejohn <joelittlejohn@gmail.com>
2016-09-04 21:28:38 +0100

    Add deploy config

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.h2database/h2 "1.4.192"]]
+  :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true)
